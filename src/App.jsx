146b9ffca7a1d5d5b55862a2d0377ea3937b5a7f
Avichal Kaushik <avichalkaushik11@gmail.com>
2023-07-12 15:46:31 +0530

    Added Chatroom UI!

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './components/pages/Login'
+import Chatroom from './components/pages/Chatroom'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={<Login />} />
+        <Route exact path='/' element={<Chatroom />} />
+        <Route exact path='/login' element={<Login />} />
       </Routes>
     </Router>
   )
